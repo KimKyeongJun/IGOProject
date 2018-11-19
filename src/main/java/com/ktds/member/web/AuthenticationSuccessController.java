476b7aa4e06592fa3cecabb12503960b0d2cb542
DESKTOP-05VV0CS\KKJ <rudwnsdl93@naver.com>
2018-11-19 16:46:23 +0900

    파일 업로드

--- src/main/java/com/ktds/member/web/AuthenticationSuccessController.java
+++ src/main/java/com/ktds/member/web/AuthenticationSuccessController.java
@@ -24,6 +24,7 @@ import com.ktds.member.vo.MemberVO;
 @Component("authenticationHandler")
 public class AuthenticationSuccessController implements AuthenticationSuccessHandler, AuthenticationFailureHandler {
 	
+	@Autowired
 	private MemberService memberService;
 	
 	
